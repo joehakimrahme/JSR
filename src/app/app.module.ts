62162f2e5573d1a0b0bc45d7a1cfe2266ea5681f
Joe H. Rahme <jhakimra@redhat.com>
2018-07-08 14:07:17 +0200

    Adds a paragraph to explain the rules of the game
    Done as a Angular component, no idea if it should've been a separate
    paragraph in app.component.html? Time will tell...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouletteComponent } from './roulette/roulette.component';
+import { RulesComponent } from './rules/rules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RouletteComponent
+    RouletteComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule
