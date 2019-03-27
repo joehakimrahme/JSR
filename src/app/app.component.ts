082de9766a1b1bf9703c2901ff47fed2e28bbd11
Joe H. Rahme <jhakimra@redhat.com>
2019-03-28 00:02:21 +0100

    Revoke old title
    I'm keeping "Joe" for now.
    
    I'm also committing docs/ ... not sure about this.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Mood\'s Skyrim Roulette';
+    title = 'Joe\'s Skyrim Roulette';
 }
