0d6123dc50f2afed1b7faf8ea2c80c3aceebeae5
Joe H. Rahme <jhakimra@redhat.com>
2019-04-03 00:20:02 +0200

    Rewrites the rules
    Handicaps made no sense as its seperate tab. Rules are rewritten to
    give more details, and minor changes to the layout have been made to
    accomodate the new text.
    
    I am not a good writer, I hope these rules make sense.

--- src/roulette.js
+++ src/roulette.js
@@ -3,21 +3,12 @@ $(document).ready(function(){
   $("#navbar-roulette").click(function(event){
     $(".rules").hide();
     $(".roulette").show();
-    $(".handicaps").hide();
   });
 
   $("#navbar-rules").click(function(event){
     $(".rules").show();
     $(".roulette").hide();
-    $(".handicaps").hide();
   });
-
-  $("#navbar-handicaps").click(function(event){
-    $(".rules").hide();
-    $(".roulette").hide();
-    $(".handicaps").show();
-  });
-
   // When I click any `<a href="#" ...>` gh-pages throws 404
   // I don't use the links to href anyway, everything's done in js
   $("a").click(function(event){
