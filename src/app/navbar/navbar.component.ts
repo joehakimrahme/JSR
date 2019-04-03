69dbc2bfeb0510a54bb3391447be52e853df011e
Joe H. Rahme <jhakimra@redhat.com>
2019-04-03 19:18:29 +0200

    Drops the jquery dependency
    Still some crap to remove from package.json though

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    ngOnSelect(selected: string) {
+        let _deselected, _selected_div, _deselected_div;
+
+        if (selected === 'rules') {
+            _deselected = 'roulette';
+        } else {
+            _deselected = 'rules';
+        }
+
+        _selected_div = document.getElementsByClassName(selected)[0];
+        _deselected_div = document.getElementsByClassName(_deselected)[0];
+
+        _selected_div.style['display'] = 'block';
+        _deselected_div.style['display'] = 'none';
+    }
 }
